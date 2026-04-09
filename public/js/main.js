867caa3fa2818449b6ded7041f74ed8b0330be2a
Husnain <huxnain.cs@gmail.com>
2026-04-09 17:13:02 +0500

    Update frontend components

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
 console.log("Welcome to Shadow Token.");
 console.log("Can you find all the flags?");
 
-// TODO: change default admin password from 'shadow_admin_2024' before production release
+
 const dev_mode = true;
 if (dev_mode) {
     console.log("Dev mode is active.");
